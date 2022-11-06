8385983afc6148fc0e848c2e75bbfb6d3e0d0a12
jv-aquino <jvaquinocontact@gmail.com>
2022-11-06 16:59:32 -0300

    + improves program logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-const Asteroid = (x, y) => {
-  const getLocation = (axis) => this[axis];
-  const setLocation = (axis, value) => this[axis] = value;
+const Asteroid = () => {
+  let y = 100;
+  let x = Math.floor((Math.random() * 60) + 20);
 
-  return {getLocation, setLocation};
+  const getLocation = () => {
+    
+  };
+
+  const createUI = () => {
+    
+  };
+
+  return {getLocation};
 }
 
 const Info = (() =>  {
@@ -15,13 +23,13 @@ const Info = (() =>  {
   const getRocket = (axis) => Rocket[axis];
   const setRocket = (axis, value) => Rocket[axis] = value;
 
-  const getAsteroid = (index) => asteroids[index];
+  const getAsteroids = () => asteroids;
 
-  return {getRocket, setRocket, getAsteroid};
+  return {getRocket, setRocket, getAsteroids};
 })();
 
 const Controller = (() => {
-  const checkKey = (key) => {
+  const checkMove = (key) => {
     if (key == "ArrowLeft" || key == "a") {
       if (Info.getRocket("x") == 20) {return}
       Info.setRocket("x", Info.getRocket("x") - 5);
@@ -30,18 +38,25 @@ const Controller = (() => {
       if (Info.getRocket("x") == 80) {return}
       Info.setRocket("x", Info.getRocket("x") + 5);
     }
+
     else if (key == "ArrowUp" || key == "w") {
+      if (Info.getRocket("y") == 70) {return}
+      Info.setRocket("y", Info.getRocket("y") + 5);
+    }
+    else if (key == "ArrowDown" || key == "s") {
       if (Info.getRocket("y") == 20) {return}
       Info.setRocket("y", Info.getRocket("y") - 5);
     }
-    else if (key == "ArrowDown" || key == "s") {
-      if (Info.getRocket("y") == 80) {return}
-      Info.setRocket("y", Info.getRocket("y") + 5);
-    }
     Dom.moveRocket();
   };
+  
+  const checkCrash = () => {
+    Info.getAsteroids().forEach(asteroid => {
+      
+    });
+  }
 
-  return {checkKey};
+  return {checkMove, checkCrash};
 })()
 
 const Dom = (() => {
@@ -49,14 +64,14 @@ const Dom = (() => {
 
   const moveRocket = () => {
     rocket.style.left = Info.getRocket("x") + "%";
-    rocket.style.top = Info.getRocket("y") + "%";
+    rocket.style.bottom = Info.getRocket("y") + "%";
   }
   
-  window.addEventListener("keyup", (e) => {Controller.checkKey(e.key)});
+  window.addEventListener("keyup", (e) => {Controller.checkMove(e.key)});
 
   return {moveRocket};
 })();
 
 const Start = (() => {
-
+  setInterval(Dom.checkCrash(), 20);
 })();
